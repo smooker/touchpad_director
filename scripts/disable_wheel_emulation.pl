d0f56205bea74a493345a3ca8662d80e1674511a
Miroslav Tzonkov <miro@smooker.org>
2016-09-03 15:17:16 +0300

    forgotten touchpad enable

--- scripts/disable_wheel_emulation.pl
+++ scripts/disable_wheel_emulation.pl
@@ -9,10 +9,6 @@ my @xil = split( /\n/, `xinput list`);
 my $cnt=0;
 
 foreach my $line (@xil) {
-	if ($line =~ m/TouchPad\s+id=(\d+)/) {
-		print "$cnt\t$1\t$line\n";
-		system(qq{xinput set-prop $1 "Device Enabled" 1});
-	}
 	if ($line =~ m/TrackPoint\s+id=(\d+)/) {
 		print "$cnt\t$1\t$line\n";
 		system(qq{xinput set-prop $1 "Evdev Wheel Emulation" 0});
